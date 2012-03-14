4cfa3d7ad3ef0fb420b51f31d0aa4ee45b508aca
Andrey <andreygen@hotmail.it>
2012-03-14 23:16:50 +0100

    Mat.java aggiornato

--- poo/util/Mat.java
+++ poo/util/Mat.java
@@ -1,7 +1,15 @@
 package poo.util;
 
 public final class Mat {
+	private static double EPSILON = 1.0E-14;
 	private Mat() {}
+	public static getEpsilon() { return EPSILON; }
+	public static void setEpsilon(double EPSILON) {
+		Mat.EPSILON = EPSILON;
+	} // setEpsilon
+	public static boolean circaUguali(double x, double y) {
+		return Math.abs(x - y) <= EPSILON;
+	} // circaUguali
 	public static int mcd(int x, int y) {
 		if (x <= 0 || y <= 0) throw new IllegalArgumentException();
 		return mcd_euclide(x, y);
