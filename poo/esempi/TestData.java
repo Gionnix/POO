bf21b61ae24072e7839e5f660ccf66746c84b3be
Andrey <andreygen@hotmail.it>
2012-03-14 22:30:18 +0100

    Data.java aggiornato

--- poo/esempi/TestData.java
+++ poo/esempi/TestData.java
@@ -9,7 +9,7 @@ public class TestData {
 		System.out.println("L'anno " + today.get(Data.Elemento.ANNO) + (Data.bisestile(today.get(Data.Elemento.ANNO)) ? " " : " non ") + "è bisestile");
 		Data d = new Data(29, 2, 2012);
 		System.out.println("Giorno successivo al " + d + ": " + d.giornoDopo());
-		Data d1 = new Data(1, 3, 2012);
-		System.out.println("Giorni trascorsi tra " + d + " e " + d1 + ": " + d1.distanza(d));
+		Data d1 = new Data(13, 3, 2012);
+		System.out.println("Giorni trascorsi tra " + d1 + " e oggi: " + d1.distanza(today));
 	} // main
 } // TestData
