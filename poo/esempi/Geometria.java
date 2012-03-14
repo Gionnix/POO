5062af0b29a77272d9b0afe905def8459349dc46
Andrey <andreygen@hotmail.it>
2012-03-14 22:46:22 +0100

    Triangolo.java aggiornato

--- poo/esempi/Geometria.java
+++ poo/esempi/Geometria.java
@@ -6,18 +6,16 @@ public class Geometria {
 	public static void main(String[]args) {
 		Punto p1 = new Punto(); // origine
 		Punto p2 = new Punto(3, -7);
-		double d12 = p1.distanza(p2); // = p2.distanza(p1);
-		System.out.println("x: " + p1.getX() + "  y: " + p1.getY());
+		System.out.println("Origine: x = " + p1.getX() + "  y = " + p1.getY());
+		System.out.printf("Distanza tra " + p1 + " e " + p2 + ": %1.2f\n", p1.distanza(p2));
 		p1.sposta(2, 5);
-		System.out.println(p1.toString()); // o System.out.println(p1);
-		System.out.println(p2);
-		System.out.println("Distanza: " + d12);
 		Punto p3 = new Punto(-5, 4);
 		Triangolo t = new Triangolo(p1, p2, p3);
-		System.out.println("Triangolo: " + t);
+		System.out.println(t);
 		System.out.printf("Perimetro: %1.2f \n", t.perimetro());
 		System.out.printf("Area: %1.2f \n", t.area());
 		System.out.println("Primo vertice: " + t.getVertici()[0]);
+		System.out.println("Il triangolo è " + t.tipo());
 		Punto vertici[] = {new Punto(2, 3), new Punto(2, -2), new Punto(-4, -2), new Punto(-4, 3)};
 		Poligono p = new Poligono(vertici);
 		System.out.println("Poligono formato dai vertici: " + p + "\n Area: " + p.area() + " | Perimetro: " + p.perimetro());
--- poo/geometria/Triangolo.java
+++ poo/geometria/Triangolo.java
@@ -3,6 +3,7 @@ package poo.geometria;
 public class Triangolo {
 	private Punto p1, p2, p3;
 	private double a, b, c;
+	public enum Tipo {EQUILATERO, ISOSCELE, SCALENO}
 	public Triangolo(Punto p1, Punto p2, Punto p3) {
 		a = p1.distanza(p2); b = p2.distanza(p3); c = p3.distanza(p1); // Lati
 		if ( a + b == c || b + c == a || c + a == b ) { // Se i punti sono allineati:
@@ -30,6 +31,11 @@ public class Triangolo {
 		double s = (a + b + c) / 2;
 		return Math.sqrt(s * (s - a) * (s - b) * (s - c)); // Erone
 	} // area
+	public Tipo tipo() {
+		if (a == b && b == c) return Tipo.EQUILATERO;
+		else if (a == b || b == c || c == a) return Tipo.ISOSCELE;
+		return Tipo.SCALENO;
+	}
 	public String toString() {
 		return "Triangolo di vertici: " + p1 + " " + p2 + " " + p3;
 	} // toString
