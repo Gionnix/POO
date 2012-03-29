6fd8eaef63da162a7de211eef8206fd754420344
Andrey <andreygen@hotmail.it>
2012-03-29 22:30:23 +0200

    Implemented "determinante" in Matrix class

--- poo/util/Matrix.java
+++ poo/util/Matrix.java
@@ -98,6 +98,43 @@ public final class Matrix {
 		}
 		return new int[] {-1,-1};
 	} // sella
+	public static double determinante(double[][] m) {
+		if (!quadrata(m)) throw new IllegalArgumentException("Matrice non quadrata!");
+		int n = m.length;
+		double[][] a = new double[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				a[i][j] = m[i][j];
+		int contaScambi = 0;
+		// Triangolarizzo la matrice
+		for (int j = 0; j < n; j++) {
+			if (Mat.circaUguali(a[j][j], 0)) { // pivoting
+				int p = j + 1; int pmax = p;
+				// Cerco pivot massimo per minimizzare gli errori di approssimazione
+				for (; p < n; p++)
+					if (Math.abs(a[p][j]) > Math.abs(a[pmax][j])) pmax = p;
+				if (pmax == n) return 0; // Sistema singolare -> Determinante = 0
+				double[] tmp = a[j];
+				a[j] = a[pmax];
+				a[pmax] = tmp;
+				contaScambi++;
+			}
+			for (int i = j + 1; i < n; i++) {
+				if (!Mat.circaUguali(a[i][j], 0D)) {
+					double c = a[i][j] / a[j][j];
+					for (int k = j; k < n; k++)
+						a[i][k] -= c * a[j][k];
+				}
+			} // for i
+		} // for j
+		double det = 1;
+		// Prodotto dei pivot
+		for (int i = 0; i < n; i++)
+			det *= a[i][i];
+		// Segno del determinante
+		det *= (contaScambi % 2 == 0 ? 1 : -1);
+		return det;
+	} // determinante
 	private static boolean quadrata(double[][] m) {
 		for (int i = 0; i < m.length; i++)
 			if (m.length != m[i].length)
@@ -128,26 +165,22 @@ public final class Matrix {
 		double[][] m = new double[][] {{2,3,1},{3,4,6},{1,6,5}};
 		for (int i = 0; i < m.length; i++)
 			System.out.println(java.util.Arrays.toString(m[i]));
-		System.out.println("m è simmetrica: " + simmetrica(m));
-		System.out.println();
+		System.out.println("m è simmetrica: " + simmetrica(m) + "\n");
+		System.out.println("Determinante di m = " + String.format("%1.2f", determinante(m)) + "\n");
 		System.out.println("Trasposta di m:");
 		double[][] t = trasposta(m);
 		for (int i = 0; i < t.length; i++)
 			System.out.println(java.util.Arrays.toString(t[i]));
-		System.out.println();
-		System.out.println("Matrice somma:");
+		System.out.println("\nMatrice somma:");
 		double[][] s = add(m, t);
 		for (int i = 0; i < s.length; i++)
 			System.out.println(java.util.Arrays.toString(s[i]));
-		System.out.println();
-		System.out.println("Minore complementare (1,1) di m:");
+		System.out.println("\nMinore complementare (1,1) di m:");
 		double[][] min = Matrix.minore(m, 1, 1);
 		for (int i = 0; i < min.length; i++)
 			System.out.println(java.util.Arrays.toString(min[i]));
-		System.out.println();
-		System.out.println("Elemento 'sella' di m: " + java.util.Arrays.toString(sella(m)));
-		System.out.println();
-		System.out.println("Matrice q:");
+		System.out.println("\nElemento 'sella' di m: " + java.util.Arrays.toString(sella(m)));
+		System.out.println("\nMatrice q:");
 		double[][] q = new double[][] {	{16,3,2,13},
 						{5,10,11,8},
 						{9,6,7,12},
