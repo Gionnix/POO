d7aa9c207690b03aae266195246050f36b1e6976
Andrey <andreygen@hotmail.it>
2012-05-04 02:50:43 +0200

    Updated package poo.file

--- poo/file/MergeFile.java
+++ poo/file/MergeFile.java
@@ -13,29 +13,41 @@ public class MergeFile {
 		String nome2 = sc.nextLine();
 		System.out.print("Nome file di destinazione: ");
 		String nome3 = sc.nextLine();
-		ObjectFile<Integer> f1 = new ObjectFile<Integer>(nome1, ObjectFile.Modo.LETTURA);
-		ObjectFile<Integer> f2 = new ObjectFile<Integer>(nome2, ObjectFile.Modo.LETTURA);
-		ObjectFile<Integer> f3 = new ObjectFile<Integer>(nome3, ObjectFile.Modo.SCRITTURA);
-		int x1 = 0, x2 = 0, x3 = 0; boolean first = true;
-		while (!f1.eof() && !f2.eof()) {
-			x1 = f1.peek(); x2 = f2.peek();
-			if (x1 < x2) {
-				f1.get();
+		ObjectFile<Integer> f1 = null, f2 = null, f3 = null;
+		try {
+			f1 = new ObjectFile<Integer>(nome1, ObjectFile.Modo.LETTURA);
+			f2 = new ObjectFile<Integer>(nome2, ObjectFile.Modo.LETTURA);
+			f3 = new ObjectFile<Integer>(nome3, ObjectFile.Modo.SCRITTURA);
+			int x1 = 0, x2 = 0, x3 = 0; boolean first = true;
+			while (!f1.eof() && !f2.eof()) {
+				x1 = f1.peek(); x2 = f2.peek();
+				if (x1 < x2) {
+					f1.get();
+					if (first || x1 != x3) { f3.put(x1); x3 = x1; }
+				} else {
+					f2.get();
+					if (first || x2 != x3) { f3.put(x2); x3 = x2; }
+				}
+				first = false;
+			}
+			while (!f1.eof()) {
+				x1 = f1.peek(); f1.get();
 				if (first || x1 != x3) { f3.put(x1); x3 = x1; }
-			} else {
-				f2.get();
+			}
+			while (!f2.eof()) {
+				x2 = f2.peek(); f2.get();
 				if (first || x2 != x3) { f3.put(x2); x3 = x2; }
 			}
-			first = false;
+		} catch (Exception e) {
+			System.out.println("Errore di lettura/scrittura!");
+		} finally {
+			try {
+				if (f1 != null) f1.close();
+				if (f2 != null) f2.close();
+				if (f3 != null) f3.close();
+			} catch (IOException e) {
+				System.out.println("Errore di lettura/scrittura!");
+			}
 		}
-		while (!f1.eof()) {
-			x1 = f1.peek();
-			if (x1 != x3) { f3.put(x1); f1.get(); }
-		}
-		while (!f2.eof()) {
-			x2 = f2.peek();
-			if (x2 != x3) { f3.put(x2); f2.get(); }
-		}
-		f1.close(); f2.close(); f3.close();
 	} // main
 } // MergeFile
